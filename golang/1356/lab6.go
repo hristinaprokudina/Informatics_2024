0fafd6a0c17a4ff34c83ea0340d1098b445e8d92
343144 <dashafox952@gmail.com>
2024-10-26 22:30:38 +0300

    Prokudina_lab6

---
+++ golang/1356/lab6.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+type Pistol struct {
+	Model   string  
+	Caliber float64 
+	Ammo    int     
+}
+
+func (p *Pistol) Shoot() {
+	if p.Ammo > 0 {
+		p.Ammo-- 
+		fmt.Printf("Выстрел из %s! Осталось патронов: %d\n", p.Model, p.Ammo)
+	} else {
+		fmt.Println("Патроны закончились! Перезарядите пистолет.")
+	}
+}
+
+func (p *Pistol) Reload(ammo int) {
+	p.Ammo += ammo 
+	fmt.Printf("Пистолет %s перезаряжен! Текущее количество патронов: %d\n", p.Model, p.Ammo)
+}
+
+func (p *Pistol) Info() {
+	fmt.Printf("Модель: %s, Калибр: %.2f, Патроны: %d\n", p.Model, p.Caliber, p.Ammo)
+}
+
+func main() {
+	
+	myPistol := Pistol{
+		Model:   "TT",
+		Caliber: 9.0,
+		Ammo:    10,
+	}
+
+	myPistol.Info()
+
+	myPistol.Shoot()
+	myPistol.Shoot()
+	myPistol.Reload(5)
+	myPistol.Shoot()
+	myPistol.Info()
+}
