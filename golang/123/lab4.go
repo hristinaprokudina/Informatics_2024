856155cb334e8999dfbc3fc9ef262369c9a570ff
hristinaprokudina <hristinaprokudina@gmail.com>
2024-11-30 16:26:19 +0300

    Update lab4.go

--- golang/123/lab4.go
+++ golang/123/lab4.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 	"math"
 )
-type Result struct {
-	X float64
-	Y float64
-}
+
 func RunLab4() {
 	Xn := 0.11
 	Xk := 0.36
@@ -16,21 +13,25 @@ func RunLab4() {
 	extraValues := []float64{0.2, 0.3, 0.38, 0.43, 0.57}
 	taskB(extraValues)
 }
-func taskA(Xn float64, Xk float64, deltaX float64) []float64 {
+func taskA(Xn float64, Xk float64, deltaX float64) {
 	fmt.Println("Значения Y для диапазона:")
 	for x := Xn; x <= Xk; x += deltaX {
 		y := calculateY(x)
-		fmt.Printf("x = %.2f, Y = %.4f\n", x, y)
-	}
-}
-func taskB(values []float64,extraValues float64) []float64 {
-	fmt.Println("\nЗначения Y для дополнительных значений:")
-	for _, x := range values {
-		y := calculateY(x)
-		fmt.Printf("x = %.2f, Y = %.4f\n", x, y)
+		if !math.IsNaN(y) {
+			fmt.Printf("x = %.2f, Y = %.4f\n", x, y)
+		}
 	}
 }
 
+func taskB(values []float64) {
+	fmt.Println("\nЗначения Y для дополнительных значений:")
+	for _, x := range values {
+		y := calculateY(x)
+		if !math.IsNaN(y) {
+			fmt.Printf("x = %.2f, Y = %.4f\n", x, y)
+		}
+	}
+}
 func calculateY(x float64) float64 {
 	if x <= 0 {
 		fmt.Printf("Ошибка: логарифм не определен для x = %f\n", x)
