906b1c56a079014bf2b2fe84d5d68e7cd68d883c
hristinaprokudina <hristinaprokudina@gmail.com>
2024-11-30 09:54:55 +0300

    Update lab4.go

--- golang/123/lab4.go
+++ golang/123/lab4.go
@@ -6,11 +6,13 @@ import (
 )
 
 func RunLab4() {
-	fmt.Println(taskA(0.11, 0.36, 0.05))
+	Xn := 0.11
+	Xk := 0.36
+	deltaX := 0.05
+	taskA(Xn, Xk, deltaX)
 	extraValues := []float64{0.2, 0.3, 0.38, 0.43, 0.57}
-	fmt.Println(taskB(extraValues))
+	taskB(extraValues)
 }
-
 func taskA(Xn, Xk, deltaX float64) []float64 {
 	fmt.Println("Значения Y для диапазона:")
 	for x := Xn; x <= Xk; x += deltaX {
