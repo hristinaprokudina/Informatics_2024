2d284f1342150a0677d9c05cee5f65e0a980e697
hristinaprokudina <hristinaprokudina@gmail.com>
2024-12-02 20:42:33 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-
+lab6"isuct.ru/informatics2022/lab6"
 lab7"isuct.ru/informatics2022/lab7"
 )
 func main() {
 	fmt.Println("Прокудина Христина")
+	lab6.RunLab6()
 	lab7.Runlab7()
 
 }
