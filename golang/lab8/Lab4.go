c132bcb261dc70683159f9af7250608f1810a5c4
hristinaprokudina <hristinaprokudina@gmail.com>
2024-12-07 20:46:30 +0300

    Update Lab4.go

--- golang/lab8/Lab4.go
+++ golang/lab8/Lab4.go
@@ -11,12 +11,14 @@ import (
 func RunLab8() {
 	file, err := os.Open("input.txt")
 	if err != nil {
-	fmt.Println("Ошибка при открытии файла:", err)
+		fmt.Println("Ошибка при открытии файла:", err)
 		return
 	}
 	defer file.Close()
+
 	var values []float64
 	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		value, err := strconv.ParseFloat(line, 64)
@@ -26,23 +28,28 @@ func RunLab8() {
 		}
 		values = append(values, value)
 	}
+
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Ошибка при чтении файла:", err)
 		return
 	}
-	
-	if len(values) < 3 {
+
+	if len(values) < 2 {
 		fmt.Println("Недостаточно значений в файле")
 		return
 	}
+
 	a := values[0]
 	b := values[1]
+
 	xValues := values[2:]
-	deltaX := 0.05
-	taskA(a, b, deltaX)
-	resultsB := taskB(xValues)
-	printResults("Значения Y для дополнительных значений:", xValues, resultsB)
+
+	deltaX := 0.05 
+	taskA(a, b, deltaX) 
+	resultsB := taskB(xValues) 
+	printResults("Значения Y для дополнительных значений:", resultsB) 
 }
+
 func taskA(Xn float64, Xk float64, deltaX float64) {
 	fmt.Println("Значения Y для диапазона:")
 	for x := Xn; x <= Xk; x += deltaX {
@@ -74,9 +81,9 @@ func calculateYA(x float64) float64 {
 	return (sinCubed + cosCubed) * math.Log(x)
 }
 
-func printResults(header string, xValues []float64, results []float64) {
+func printResults(header string, results []float64) {
 	fmt.Println(header)
 	for i, y := range results {
-		fmt.Printf("x = %.2f, Y = %.4f\n", xValues[i], y)
+		fmt.Printf("x = %.2f, Y = %.4f\n", 0.2+float64(i)*0.1, y)
 	}
 }
