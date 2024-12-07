49ca78336df3740467b0c475e48f5d1186a66cb9
hristinaprokudina <hristinaprokudina@gmail.com>
2024-12-07 11:51:32 +0300

    Update lab4.go

--- golang/lab4/lab4.go
+++ golang/lab4/lab4.go
@@ -9,8 +9,10 @@ func RunLab4() {
 	Xn := 0.11
 	Xk := 0.36
 	deltaX := 0.05
+	
 	resultsA := taskA(Xn, Xk, deltaX)
 	printResults("Значения Y для диапазона:", resultsA)
+
 	extraValues := []float64{0.2, 0.3, 0.38, 0.43, 0.57}
 	resultsB := taskB(extraValues)
 	printResults("Значения Y для дополнительных значений:", resultsB)
