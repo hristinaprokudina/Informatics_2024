d3b8856f1e050e43bf97915df746cfcf325f1461
343144 <dashafox952@gmail.com>
2024-12-07 19:26:33 +0300

    Prokudina_lab8

---
+++ golang/lab8/Lab4.go
@@ -0,0 +1,89 @@
+package lab8
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func Runlab8() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Ошибка при открытии файла:", err)
+		return
+	}
+	defer file.Close()
+
+	var values []float64
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		value, err := strconv.ParseFloat(line, 64)
+		if err != nil {
+			fmt.Println("Ошибка при чтении числа:", err)
+			continue
+		}
+		values = append(values, value)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Ошибка при чтении файла:", err)
+		return
+	}
+
+	if len(values) < 2 {
+		fmt.Println("Недостаточно значений в файле")
+		return
+	}
+
+	a := values[0]
+	b := values[1]
+
+	xValues := values[2:]
+
+	deltaX := 0.05 
+	taskA(a, b, deltaX) 
+	resultsB := taskB(xValues) 
+	printResults("Значения Y для дополнительных значений:", resultsB) 
+}
+
+func taskA(Xn float64, Xk float64, deltaX float64) {
+	fmt.Println("Значения Y для диапазона:")
+	for x := Xn; x <= Xk; x += deltaX {
+		y := calculateY(x)
+		if !math.IsNaN(y) {
+			fmt.Printf("x = %.2f, Y = %.4f\n", x, y)
+		}
+	}
+}
+
+func taskB(values []float64) []float64 {
+	var results []float64
+	for _, x := range values {
+		y := calculateY(x)
+		if !math.IsNaN(y) {
+			results = append(results, y)
+		}
+	}
+	return results
+}
+
+func calculateY(x float64) float64 {
+	if x <= 0 {
+		fmt.Printf("Ошибка: логарифм не определен для x = %f\n", x)
+		return math.NaN()
+	}
+	sinCubed := math.Pow(math.Sin(x), 3)
+	cosCubed := math.Pow(math.Cos(x), 3)
+	return (sinCubed + cosCubed) * math.Log(x)
+}
+
+func printResults(header string, results []float64) {
+	fmt.Println(header)
+	for i, y := range results {
+		fmt.Printf("x = %.2f, Y = %.4f\n", 0.2+float64(i)*0.1, y)
+	}
+}
