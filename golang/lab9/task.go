2774a76b1775de187c1f8e25a257166336dcdd74
343144 <dashafox952@gmail.com>
2024-12-24 16:18:08 +0300

    Prokudina lab9

---
+++ golang/lab9/task.go
@@ -0,0 +1,14 @@
+<<<<<<< HEAD:golang/lab9/task.go
+package lab9
+
+type Task struct {
+	Description string 'json:"description"
+	Completed bool 'json:"completed"
+=======
+package lab9
+
+type Task struct {
+	Description string 'json:"description"
+	Completed bool 'json:"completed"
+>>>>>>> 36b7497c851a67dcc78dfcc665b81c4ffe622e9d:task.go
+}
